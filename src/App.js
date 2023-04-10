5cd58a6f6cbdad72a9b1fad6869f08b15a63ec9e
StuartByers <stuart.byers@outlook.com>
2023-04-10 22:39:49 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { name: "Buy shopping", id: 1 },
+    { name: "Clean bathroom", id: 2 },
+    { name: "Car's MOT", id: 3 },
+  ]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleTaskSubmit = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    if (newTask) {
+      copyTasks.push({name: newTask, id: Date.now()});
+      setTasks(copyTasks);
+      setNewTask('');
+    }
+  };
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do List</h1>
+
+      <ul>
+        {tasks.map(task => {
+          return (
+            <li key={task.id}><span>{task.name}</span></li>
+          );
+        })}
+      </ul>
+
+      <form onSubmit={handleTaskSubmit}>
+        <label htmlFor="new-task">Add a new task:</label>
+        <input type="text" id="new-task" onChange={handleTaskInput} value={newTask} />
+        <button>Save Task</button>
+      </form>
     </div>
   );
 }
